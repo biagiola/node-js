2ebc7fe7a8a5029efe833ba9602dd2e9a5d3d885
David <41311088+biagiola@users.noreply.github.com>
2019-12-19 20:31:01 -0300

    Add files via upload

--- 02_fileSystem/03mkdir.js
+++ 02_fileSystem/03mkdir.js
@@ -1,17 +1,20 @@
 var fs = require('fs');
 
 /*Sync*/
+    console.log('Sync way');
     // sync way to add dir 
-    fs.mkdirSync('Stuff', function(){
-        console.log('Directory made');
-    });
-    // sync way to remove dir 
-    fs.rmdirSync('Stuff', function(){
+    /* fs.mkdirSync('Stuff', function(){
         console.log('Directory made');
     }); 
 
+    // sync way to remove dir 
+    fs.rmdirSync('Stuff', function(){
+        console.log('Directory made');
+    });  */
+
 
 /*Async*/
+    console.log('\nAync way');
     // async way to make a dir (with a file 'message.txt' inside)
     fs.mkdir('stuff', function(){
         fs.readFile('readme.txt', 'utf8', function(err, data){
@@ -21,7 +24,8 @@ var fs = require('fs');
         })
     })
 
+
     // delete a dir (include the file inside)
     fs.unlink('./stuff/readme.txt', function(){
         fs.rmdir('./stuff', function(){});
-    });
+    }); 
