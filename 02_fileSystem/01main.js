19d631f7bc7424731a35cdde180c4ab60e53a081
David <41311088+biagiola@users.noreply.github.com>
2019-12-19 20:01:20 -0300

    Add files via upload

--- 02_fileSystem/01main.js
+++ 02_fileSystem/01main.js
@@ -3,37 +3,48 @@ var fs = require('fs');
 
 /*Syncronize way*/
     // sync way to read dir data 
-    const filesTwo = fs.readdirSync('../');
-    console.log('Datos de un directorio Sync \n', filesTwo);
+    // const upperDirFiles = fs.readdirSync('.../'); // upper directory
+    console.log( 'Syncronize way ');
+    const actualDirFiles = fs.readdirSync('./'); // actual directory
+    console.log( 'Actual directory Sync \n', actualDirFiles );
 
     // sync way to read a file 
-    var readMe = fs.readFileSync('readme.txt', 'utf8');
-    console.log('Archivo readme.txt => ',readMe);
+    var readMe = fs.readFileSync( 'readme.txt', 'utf8' );
+    //console.log( 'File: readme.txt => ', readMe );
+    console.log( `File: readme.txt => ${ readMe }` );
 
     // sync way to write/copy a file
     fs.writeFileSync('readmeCopy.txt', readMe);
 
 /*Asyncronize way*/
-    // async way to read dir data 
-    const files = fs.readdir('../', function(err, files){
-        if (err) console.log('Error', err);
-        else console.log('Result', files);
+    console.log( '\nAsyncronize way' );
+    // async way to read dir data of actual directory 
+    const files = fs.readdir( './', function( err, files ) {
+        if ( err ) console.log('Error', err);
+        else console.log( 'Actual directory Async\n', files );
     });
     
     // async way to write a file 
-    var data = 'hola que tal';
+    var data = 'Hi there, this is a copy';
     fs.writeFile('message.txt', data, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
+        if (err) throw err;
+        console.log('The file has been saved!');
     });
 
     // async way to write/copy a file
-    fs.readFile('readme.txt', 'utf8', function(err, a){
-        fs.writeFile('readmeCopy2.txt', a);
-    });
+    fs.readFile('readme.txt', 'utf8', function( err, a ) {
+
+        /* fs.writeFile( 'readmeCopy2.txt', a)  */
+
+        fs.writeFile('Copy-Of-message.txt', data, (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
+    }); 
 
     //To delete files
     //fs.unlink('readmeCopy.txt');
+
 
 
 
